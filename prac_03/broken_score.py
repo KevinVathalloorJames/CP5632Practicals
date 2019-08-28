4e587059cfaee66c705d9a652229482a9e0ab491
KevinVathalloorJames <kevin.vathalloorjames@my.jcu.edu.au>
2019-08-28 18:23:31 +1000

    broken_score with function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,12 +1,20 @@
-
-score = float(input("Enter score: "))
-while score < 0:
-    print("invalid score")
+def main():
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Fail")
+    while score < 0:
+        print("invalid score")
+        score = float(input("Enter score: "))
+    print(validate_score(score))
+
+
+def validate_score(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Fail"
+
+
+main()
+
 
