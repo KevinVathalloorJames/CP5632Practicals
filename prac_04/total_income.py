5d8bf971ef47660c002cca04eea09df843a91f9a
KevinVathalloorJames <kevin.vathalloorjames@my.jcu.edu.au>
2019-08-29 12:10:02 +1000

    total income program with function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -3,7 +3,6 @@ CP1404/CP5632 Practical - Suggested Solution
 Cumulative total income program
 """
 
-
 def main():
     """Display income report for incomes over a given number of months."""
     incomes_per_month = []
