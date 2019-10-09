25ff6b2c927f38ef77b909b4f8643757a10995b1
KevinVathalloorJames <kevin.vathalloorjames@my.jcu.edu.au>
2019-10-09 17:33:11 +1000

    miles to kilometer

---
+++ prac_07/ml_to_km.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+MILE_KM = 1.60934
+
+
+class MileConverter(App):
+
+    def build(self):
+        Window.size = (300, 200)
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('m_km.kv')
+        return self.root
+
+    def get_miles(self):
+        try:
+            mile_value = float(self.root.ids.miles.text)
+            return mile_value
+        except ValueError:
+            return 0
+
+    def handle_calculate(self):
+        mile_value = self.get_miles()
+        km_value = mile_value * MILE_KM
+        self.root.ids.output_label.text = str(km_value)
+
+    def handle_increment(self, increment_value):
+        mile_value = self.get_miles() + increment_value
+        self.root.ids.miles.text = str(mile_value)
+        self.handle_calculate()
+
+
+MileConverter().run()
